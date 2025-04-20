3fde9502572e9831cf5e442662e7c8a63a3cd0a2
Yuto Tanaka <karaage_nennen@icloud.com>
2025-04-20 17:16:02 +0900

    wip

---
+++ plugins/tabline-wez/init.lua
@@ -0,0 +1,49 @@
+local wezterm = require("wezterm")
+
+local options = {
+    icons_enabled = true,
+    theme = "GitHub Dark",
+    tabs_enabled = true,
+    theme_overrides = {},
+    section_separators = {
+        left = wezterm.nerdfonts.pl_left_hard_divider,
+        right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+    component_separators = {
+        left = wezterm.nerdfonts.pl_left_soft_divider,
+        right = wezterm.nerdfonts.pl_right_soft_divider,
+    },
+    tab_separators = {
+        left = wezterm.nerdfonts.pl_left_hard_divider,
+        right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+}
+
+local sections = {
+    tabline_a = { "mode" },
+    tabline_b = { "workspace" },
+    tabline_c = { " " },
+    tab_active = {
+        "index",
+        { "parent", padding = 0 },
+        "/",
+        { "cwd", padding = { left = 0, right = 1 } },
+        { "zoomed", padding = 0 },
+    },
+    tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
+    tabline_x = { "ram", "cpu" },
+    tabline_y = { "datetime", "battery" },
+    tabline_z = { "domain" },
+}
+
+local extensions = {
+    -- TODO: it
+}
+
+local opts_tabline = {
+    options = options,
+    sections = sections,
+    extensions = extensions,
+}
+
+return opts_tabline
