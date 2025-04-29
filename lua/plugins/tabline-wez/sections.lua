4e8904602bde13490247c84f317f1d7a966e370b
Yuto Tanaka <karaage_nennen@icloud.com>
2025-04-30 08:37:18 +0900

    updated

--- lua/plugins/tabline-wez/sections.lua
+++ lua/plugins/tabline-wez/sections.lua
@@ -1,40 +1,5 @@
 local wezterm = require("wezterm")
 local nf = wezterm.nerdfonts
-
-local SPACE_1 = " "
-local normal_key_icon = nf.md_alphabetical_variant .. SPACE_1
-local leader_key_icon = nf.md_keyboard_outline .. SPACE_1
-local ime_icon = nf.md_syllabary_hiragana .. SPACE_1
-
-return function(config, battery)
-    return {
-        tabline_a = { "mode" },
-        tabline_b = { "workspace" },
-        tabline_c = { SPACE_1 },
-        tab_active = {
-            "index",
-            { "parent", padding = 0 },
-            "/",
-            { "cwd", padding = { left = 0, right = 1 } },
-            { "zoomed", padding = 0 },
-        },
-        tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
-        tabline_x = { { "ram" }, { "cpu" } },
-        tabline_y = {
-            {
-                "datetime",
-                cond = function()
-                    return false
-                end,
-            },
-        },
-        tabline_z = { { "battery" } },
-    }
-end
-
---[[
-local wezterm = require("wezterm")
-local nf = wezterm.nerdfonts
 local normal_key_icon = nf.md_alphabetical_variant .. " "
 local leader_key_icon = nf.md_keyboard_outline .. " "
 local ime_icon = nf.md_syllabary_hiragana .. " "
@@ -141,4 +106,3 @@ return function(config, battery)
         },
     }
 end
-]]
